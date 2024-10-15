461372262cf023916d92a994d670d4532d8f2c69
Steffen Tautenhahn <stevie-@users.noreply.github.com>
2024-10-15 17:39:19 +0200

    fix: disable auto git configure
    Signed-off-by: Steffen Tautenhahn <stevie-@users.noreply.github.com>

--- git/install.sh
+++ git/install.sh
@@ -5,21 +5,21 @@ set -e
 echo "🚀 git configuration"
 
 # Check if the file exists and it RUNNING_IN_DEV_CONTAINER is false
-if [ ! -f "${HOME}/.gitconfig" ] && [ -z "${RUNNING_IN_DEV_CONTAINER}" ]; then
-  cp "${PWD}/git/.gitconfig.local" "${HOME}/.gitconfig.local"
+# if [ ! -f "${HOME}/.gitconfig" ] && [ -z "${RUNNING_IN_DEV_CONTAINER}" ]; then
+#   cp "${PWD}/git/.gitconfig.local" "${HOME}/.gitconfig.local"
 
-  echo "Enter your full name";
-  read -r var
-  sed -i '' "s|GITNAME|${var}|" "${HOME}/.gitconfig.local"
+#   echo "Enter your full name";
+#   read -r var
+#   sed -i '' "s|GITNAME|${var}|" "${HOME}/.gitconfig.local"
 
-  echo "Enter your email address";
-  read -r var
-  sed -i '' "s|GITEMAIL|${var}|" "${HOME}/.gitconfig.local"
+#   echo "Enter your email address";
+#   read -r var
+#   sed -i '' "s|GITEMAIL|${var}|" "${HOME}/.gitconfig.local"
 
-  echo "Entire the path to your public ssh key"
-  read -r var
-  sed -i '' "s|GITSIGNKEY|${var}|" "${HOME}/.gitconfig.local"
-fi
+#   echo "Entire the path to your public ssh key"
+#   read -r var
+#   sed -i '' "s|GITSIGNKEY|${var}|" "${HOME}/.gitconfig.local"
+# fi
 
 ln -sf "${PWD}/git/.gitconfig" "${HOME}/.gitconfig"
 ln -sf "${PWD}/git/.gitignore" "${HOME}/.gitignore"
